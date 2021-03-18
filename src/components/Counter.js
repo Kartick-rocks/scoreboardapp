ee3bae118e0230d2bda6414940175eef2e457c72
Kartick Sridhar <sridharkartick29@gmail.com>
2021-03-18 10:49:39 -0700

    Scoreboard app pushed

---
+++ src/components/Counter.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const  Counter = (props) => {
+
+  /*constructor() {
+    super()  //We call super() in order to call the constructor of the component class
+    this.state = {
+      score: 0
+    };
+
+  }*/
+
+
+
+  // incrementScore() {
+  //  this.setState( prevState => {
+  //    return {
+  //    score: prevState.score + 1
+  //  }
+  //  });
+  // }
+  //
+  // decrementScore() {
+  //  this.setState( prevState => {
+  //    return {
+  //    score: prevState.score - 1
+  //  }
+  //  });
+  // }
+
+
+
+    return (
+      <div className = "counter">
+      <button  className="counter-action-decrement" > - </button>
+      <span className="counter-score"> {props.score} </span>
+      <button  className="counter-action-increment" > + </button>
+      </div>
+    );
+  
+}
+
+export default Counter;
