11b6f3f084397e2b75271d28c7ff71c623ca9520
Kartick-rocks <sridharkartick29@gmail.com>
2021-03-18 18:31:26 -0700

    Add files via upload

---
+++ AddPlayerForm.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+class AddPlayerForm extends Component {
+
+  state = {
+    value: ''
+  };
+
+  handleValueChange = (e) => {
+    this.setState({value: e.target.value});
+
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault(); //If you don't prevent the form's default behaviour, it will again reload from scratch which means we would loose all the application's data
+    this.props.addPlayer(this.state.value);
+    this.setState({ value: ''});
+  }
+
+
+  render () {
+    console.log(this.state.value);
+    return (
+      <form onSubmit={this.handleSubmit}>
+      <input
+       type="text
+       value = {this.state.value}
+       onChange = {this.handleValueChange}
+       placeholder="Enter a player's name"
+      />
+
+      <input
+       type="submit"
+       value="Add player"
+      />
+      </form>
+
+
+    );
+  }
+}
+
+export default AddPlayerForm;
