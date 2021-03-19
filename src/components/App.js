33efda7ec989f30e9d0dab9ba35561a9bcf16f47
Kartick-rocks <sridharkartick29@gmail.com>
2021-03-18 18:37:48 -0700

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -108,10 +108,27 @@ class App extends Component {
 
   };
 
+  prevPlayerId = 4;
+
   handleScoreChange = (index, delta) => {
     this.setState( prevState => ({
       score: prevState.players[index].score += delta
     }));
+   }
+
+   handleAddPlayer = (name) => {
+     this.setState({
+       players: [
+         ...this.state.players,
+         {
+           name,
+           score: 0,
+           id: this.prevPlayerId += 1
+         }
+
+       ]
+     });
+
    }
 
 
@@ -146,7 +163,7 @@ class App extends Component {
 
       )}
 
-      <AddPlayerForm />
+      <AddPlayerForm addPlayer={this.handleAddPlayer}/>
       </div>
     );
 
